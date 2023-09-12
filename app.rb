0101e8501fcb68beb5e42653ae0919adbfa9050d
Alina Sapronova <106851105+AlinaSapronova@users.noreply.github.com>
2023-09-12 18:32:12 +0000

    finished with umbrella part

--- app.rb
+++ app.rb
@@ -1,5 +1,9 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+
+
+
 
 get("/") do
 erb:home
@@ -11,6 +15,33 @@ end
 
 post("/process_umbrella") do
   @user_location = params.fetch("user_loc")
+  encoder =  @user_location.gsub(" ", "+")
+
+  gmaps_url="https://maps.googleapis.com/maps/api/geocode/json?address=#{encoder}&key=AIzaSyDKz4Y3bvrTsWpPRNn9ab55OkmcwZxLOHI"
+  
+  @result = HTTP.get(gmaps_url).to_s
+  @parsed_result = JSON.parse(@result)
+  @loc_hash = @parsed_result.dig("results", 0, "geometry", "location")
+  @lat = @loc_hash.fetch("lat")
+  @lng = @loc_hash.fetch("lng")
+
+  pirate_weather = "https://api.pirateweather.net/forecast/3RrQrvLmiUayQ84JSxL8D2aXw99yRKlx1N4qFDUE/#{@lat},#{@lng}"
+
+  @weather_result = HTTP.get(pirate_weather)
+  response = JSON.parse(@weather_result)
+  @currently = response.fetch("currently")
+  @temperature = @currently.fetch("temperature")
+  @summary = @currently.fetch("summary")
+
+
+  precip_probability = @currently.fetch("precipProbability")
+
+  if precip_probability > 0.10
+    @result = "You might want to take an umbrella!"
+  else
+    @result = "You probably won't need an umbrella."  
+  end
+ 
   erb:umbrella_response
 end
 
