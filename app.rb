e85e979bf228f9cb91e0dd090648dcf5003bf20c
Alina Sapronova <106851105+AlinaSapronova@users.noreply.github.com>
2023-09-12 17:25:13 +0000

    get user location in umbrella part

--- app.rb
+++ app.rb
@@ -2,12 +2,26 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+erb:home
 end
 
 get("/umbrella") do
   erb:umbrella_form
+end
+
+post("/process_umbrella") do
+  @user_location = params.fetch("user_loc")
+  erb:umbrella_response
+end
+
+get("/message") do
+  erb:ai_message_form
+end
+
+get("/process_single_message") do
+  erb(:message_response)
+end
+
+get("/chat") do
+  erb:ai_chat_form
 end
